66c953942ee0ad9bbaa1deec2520dd41b96990d3
Yang Lingyuan <yangly219@gmail.com>
2019-04-30 11:14:40 +0200

    toolbox implementation, debugged controller and cleaned up work space

--- ADMM.m
+++ ADMM.m
@@ -1,61 +1,82 @@
 clc
 clear;
-[X,Y]=meshgrid(-10:0.1:10);
+load A2;
+ICR_ref(1)=-20;
+ICR_ref(2)=20;
+XP=-20:0.1:20;
 
-%QP problem specification
-Xref=[1,1];
-P=[1,0;0,1];
-q=[-Xref(1),-Xref(2)]';
-r=Xref(1)^2+Xref(2)^2;
-%inequality constraint
-%to keep the form Ax+b>0
-A1=[1,-1];
-b1=2;
-A2=[-1,1];
-b2=2;
-A3=[-1,-1];
-b3=2;
-A4=[1,1];
-b4=2;
-A=[A1;A2;A3;A4];
-b=[b1;b2;b3;b4];
-%updating variables
-%x_init=[-3,5]';
-x_new=Xref';
+r=ICR_ref(1)^2+ICR_ref(2)^2;
+hx1 = 0.263;
+hx2 = 0.263; 
+hx3 = -0.263;
+hx4 = -0.263;
+
+hy1 = -0.263;
+hy2 = 0.263;
+hy3 = 0.263;
+hy4 = -0.263;
+
+hxi =[hx1, hx2, hx3, hx4];
+hyi =[hy1, hy2, hy3, hy4];
+h_i=[hxi(1),hyi(1);hxi(2),hyi(2);hxi(3),hyi(3);hxi(4),hyi(4)];
+
+
+% A(:,1)=A(:,1)./100;
+
+
+figure(1);
+% contour(X,Y,F,50);
+% xlabel('x');
+% ylabel('y');
+hold on;
+ for i=1:8
+    if(A(i,2)>0)
+        YP=-A(i,1)*XP + b(i);
+    else
+        YP=A(i,1)*XP - b(i);
+    end
+%     %             YP=-cot(Beta(i))*XP + cot(Beta(i))*h_i(i,1)+h_i(i,2);
+     plot(XP,YP);
+     hold on;
+ end
+hold on;
+scatter(h_i(1,1),h_i(1,2));
+hold on;
+scatter(h_i(2,1),h_i(2,2));
+hold on;
+scatter(h_i(3,1),h_i(3,2));
+hold on;
+scatter(h_i(4,1),h_i(4,2));
+hold on;
+scatter(ICR_ref(1),ICR_ref(2),"filled");
+hold on;
+
+P = [1,0;0,1];
+q = [-ICR_ref(1),-ICR_ref(2)]';
+
+
+x_new=ICR_ref';
 u_new=zeros(size(A,1),1);
 z_new=zeros(size(A,1),1);
-Max_Iterate=1000;
+
+Max_Iterate=500;
 rho=1;
 ABSTOL= sqrt(2)*1e-4;
 RELTOL= 1e-2;
 
-%ploting related
-history_x1=[x_new(1),zeros(1,999)];
-history_x2=[x_new(2),zeros(1,999)];
-F=P(1,1)*X.^2+P(1,2)*X.*Y+P(2,1)*X.*Y+P(2,2)*Y.^2 + q(1)*X+q(2)*Y + r;
-f=zeros(1,999);
 
-Y_c1=-A1(1)/A1(2)*X(1,:)-sign(A1(2))*b1;
-Y_c2=-A2(1)/A2(2)*X(1,:)-sign(A2(2))*b2;
-Y_c3=-A3(1)/A3(2)*X(1,:)-sign(A3(2))*b3;
-Y_c4=-A4(1)/A4(2)*X(1,:)-sign(A4(2))*b4;
 
 figure(1);
-contour(X,Y,F,50);
-xlabel('x');
-ylabel('y');
-hold on;
-plot(X(1,:),Y_c1,X(1,:),Y_c2,X(1,:),Y_c3,X(1,:),Y_c4);
-axis([-10 10 -10 10]);
-hold on;
-N = -(P + rho*(A'*A));
-N2 = N\(rho*A');
-N1 = N\(q - rho*A'*b);
-tic
+
+% N = -(P + rho*(A'*A));
+% N2 = N\(rho*A');
+% N1 = N\(q - rho*A'*b);
+
 for k=1:Max_Iterate
     
     %x-update
-    x_new=N1 + N2*(z_new + u_new);
+    x_new=-inv(P+rho*A'*A)*(q+rho*A'  * ( z_new + u_new - b ) );
+%     x_new=N1 + N2*(z_new + u_new);
     %z-update
     z_old=z_new;
     z_new = max(0, -A*x_new - u_new + b);
@@ -66,13 +87,13 @@ for k=1:Max_Iterate
     
     %cost function
     %f(k+1)=x_new'*P*x_new + q'*x_new + r;
-    history_x1(k+1)=x_new(1);
-    history_x2(k+1)=x_new(2);
-    
-    plot(history_x1(k+1),history_x2(k+1),'r*');
-    hold on;
-    plot(history_x1(k:k+1),history_x2(k:k+1),'r');
-    hold on;
+%     history_x1(k+1)=x_new(1);
+%     history_x2(k+1)=x_new(2);
+%     
+%     plot(history_x1(k+1),history_x2(k+1),'r*');
+%     hold on;
+%     plot(history_x1(k:k+1),history_x2(k:k+1),'r');
+%     hold on;
     
     %termination check
     primal_residual=norm(A*x_new - b + z_new);
@@ -83,4 +104,4 @@ for k=1:Max_Iterate
         break;
     end
 end
-toc
+scatter(x_new(1),x_new(2));
